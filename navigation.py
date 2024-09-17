2b2c8e1dd80e6750c8e799ce1d5f28556688a152
Ameer <79278779+meroo73@users.noreply.github.com>
2024-09-17 21:29:18 +0200

    Add files via upload

---
+++ navigation.py
@@ -0,0 +1,62 @@
+import pyautogui as py
+import time
+
+# # # Wait for 4 seconds
+time.sleep(4)
+
+#Get the current position of the mouse cursor
+position = py.position()
+# Print the position
+print(f"Mouse position: {position}")
+
+
+# py.scroll(-850)
+
+# py.hotkey('ctrl', '+')
+# time.sleep(0.3)
+# py.hotkey('ctrl', '+')
+# time.sleep(0.3)
+# py.hotkey('ctrl', '+')
+# time.sleep(0.3)
+# py.hotkey('ctrl', '+')
+# time.sleep(0.3)
+# py.hotkey('ctrl', '+')
+
+
+
+# py.click(x=756, y=566)
+# py.press('tab')
+# py.press('down')
+# py.press('down')
+
+
+time.sleep(1)
+
+py.moveTo(x=459,y=882)
+
+
+
+
+# Wait for the website to load
+# time.sleep(5)
+
+
+# # Press Ctrl + F to open the browser's search box
+# py.hotkey('ctrl', 'f')
+
+# # Wait for the find box to appear
+# time.sleep(1)
+
+# # Type the word you're searching for
+# search_word = " schon mal eine Thrombose"
+# py.write(search_word)
+# time.sleep(1)
+# py.press("esc")
+# py.press("tab")
+# py.press("right")
+
+
+# Press Enter to initiate the search
+
+
+
